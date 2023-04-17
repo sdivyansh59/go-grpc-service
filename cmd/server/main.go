02995e5670963f8bf6d8958bfac8c1f6649a7fe0
Divyansh Singh <sdivyansh59@gmail.com>
2023-04-17 19:50:31 +0530

    migration is not working/ table is not creted in container

--- cmd/server/main.go
+++ cmd/server/main.go
@@ -1,15 +1,35 @@
 package main
 
-import "log"
+import (
+	"log"
+
+	"github.com/sdivyansh59/go-grpc-service/internal/db"
+	"github.com/sdivyansh59/go-grpc-service/internal/rocket"
+)
 
 //pkgm
 
 func Run() error {
 	// responsible for initializing and starting
 	// out gRPC server
-	return nil
+	rocketStore, err := db.New()
+	if err != nil {
+		return err
+	}
+
+	err = rocketStore.Migrate()
+	if err != nil {
+		log.Println("Failed to run migrations")
+		return err
+	}
+	
+	_ = rocket.New(rocketStore)
+
+	return nil 
 }
  
+
+
 func main() {
 	if err := Run(); err != nil {
 		log.Fatal(err)
